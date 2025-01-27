bacacf3817b45142a529fe75e63a2ceb541e2262
harun-u-r-rashid <hr100756@gmail.com>
2025-01-27 18:02:41 +0600

    Update

--- src/components/Trx.jsx
+++ src/components/Trx.jsx
@@ -16,6 +16,8 @@ function Trx() {
 
         const [history, setHistory] = useState([]);
 
+        const [withdrawHistory, setWithdrawHistory] = useState([]);
+
         const fetchDepositHistory = async () => {
                 try {
                         // https://sk-money-save-u6f9.onrender.com/
@@ -29,8 +31,23 @@ function Trx() {
                 }
         };
 
+
+        const fetchWithdrawHistory = async () => {
+                try {
+                        // https://sk-money-save-u6f9.onrender.com/
+                        // const res = await axiosInstance.get(`http://127.0.0.1:8000/package/withdraw_history/`);
+                        const res = await axiosInstance.get(`https://sk-money-save-u6f9.onrender.com/package/withdraw_history/`);
+                        setHistory(res.data);
+                        console.log(res.data);
+
+                } catch (error) {
+                        console.log(error);
+                }
+        };
+
         useEffect(() => {
                 fetchDepositHistory();
+                fetchWithdrawHistory()
         }, []);
 
         return (
@@ -60,6 +77,8 @@ function Trx() {
 
                                 <section class=" mx-2">
                                         <div class="max-w-7xl mx-auto rounded-lg shadow-lg mt-5  flex flex-col gap-2">
+
+                                                <h1 className='text-center font-bold'>Your all deposit request</h1>
 
 
                                                 {history?.map((h, index) => (
@@ -94,6 +113,51 @@ function Trx() {
 
 
                                         </div>
+
+                                      
+                                        
+                                        
+                                          <div class="max-w-7xl mx-auto rounded-lg shadow-lg mt-5  flex flex-col gap-2">
+
+                                                <h1>Your all withdraw request</h1>
+
+
+                                                {withdrawHistory?.map((wh, index) => (
+
+                                                        <div class="bg-white flex font-bold items-center justify-between gap-2 mt-2 p-4">
+                                                                <div class="  flex items-center justify-start gap-2   rounded-full ">
+                                                                        <div class="border bg-[rgb(33,54,68)] w-10 h-10 rounded-full flex items-center justify-center p-2 ">
+                                                                                <i class="text-white fa-solid fa-arrow-right"></i>
+                                                                        </div>
+
+
+                                                                        <span class="text-[rgb(185,28,28)] text-[10px]">{wh?.b_number}</span>
+                                                                </div>
+                                                                <div class="flex flex-col items-center">
+                                                                        <h1>{wh?.amount}$</h1>
+                                                                        <span class="border bg-[rgb(33,54,68)] px-1 text-[12px] text-white rounded-md">
+                                                                                {wh?.status}
+                                                                        </span>
+                                                                </div>
+
+
+                                                        </div>
+
+                                                ))}
+
+
+
+
+
+
+
+
+
+                                        </div>
+
+
+                                        
+                                       
 
 
 
